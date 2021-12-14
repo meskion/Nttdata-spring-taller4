0b38601618be5761dcafd77134c80cd503f3ed35
unknown <manufdh@hotmail.com>
2021-12-14 08:31:27 +0100

    first commit

---
+++ src/main/java/com/nttdata/spring/NttDataSpringMain.java
@@ -0,0 +1,14 @@
+package com.nttdata.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttDataSpringMain {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttDataSpringMain.class, args);
+	}
+
+}
